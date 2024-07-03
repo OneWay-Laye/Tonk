5ed84e4fce8e711108fcbac12d61ada12ed7bb45
Alayé Taylor <54966515+OneWay-Laye@users.noreply.github.com>
2024-07-03 05:59:33 -0400

    This commit will work on the first round ending at player 1 choosing card to discard.

--- gameFunctions.py
+++ gameFunctions.py
@@ -37,6 +37,33 @@ def dealHands(deck):
             player2Hand.append(randomCard)
             deck.remove(randomCard)
             cardsLeft -= 1
-    print(f' player 1\' hand is {player1Hand}')
-    print(f' player 2\' hand is {player2Hand}')
-    return player1Hand
+    
+    return player1Hand, player2Hand
+
+
+
+# this will be the code for each round.
+def playerTurn(gameOver, round, deck, player1Hand, player2Hand):
+    turn = 'player1'
+    #while not gameOver:
+    cardsLeft = 41
+    if (round % 2 != 0) and gameOver == False:
+        print('Player 1 its your turn.')
+        print(f'Your cards are {player1Hand}')
+        if round == 1:
+            print('Since its the first round you must draw from the deck to start the game.')
+            # selects the Number card
+            randomCardPosition = randint(0, cardsLeft)
+
+            #sets it to the card in the deck
+            randomCard = deck[randomCardPosition]
+            player1Hand.append(randomCard)
+            deck.remove(randomCard)
+            cardsLeft -= 1
+            print(f'You now have these 6 cards {player1Hand}')
+            cardToDiscard = input('Enter the card that you want to discard: ')
+            print(cardToDiscard)
+    else:
+        print('its player twos turn')
+
+    return
